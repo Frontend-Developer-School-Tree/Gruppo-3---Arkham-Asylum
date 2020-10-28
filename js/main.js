e1b8aec1afdd07c1509b0d9c1a32ff14a3c88522
Giuseppe Ceniviva <cenipeppe@outlook.it>
2020-10-28 16:24:15 +0100

    Modifiche Pomeriggio 1

--- js/main.js
+++ js/main.js
@@ -1,15 +1,54 @@
+const containerDetenuto = document.getElementById('containerDetenuti');
+const tplDet = document.getElementById('tplDetenuti');
+const urlDet = '../db/detenuti.json'
+
 class Detenuto {
     static async getDetenuto(){
-        try {
+
+        // VERSIONE GRUPPO
+        //try {
             // console.log('ok')
             const response = await fetch('../db/detenuti.json');
             const data = await response.json();
-            console.log(data);
-        } catch (error) {
-            console.error(error);
-        }
+            // console.log(data);
+
+            data.map(detenuto => {
+                const cardDet = document.importNode(tplDet.content, true);
+                // console.log(detenuto.nomePersonaggio)
+                
+                cardDet.querySelector('.nome').textContent= detenuto.nomePersonaggio;
+                cardDet.querySelector('.razza').textContent = 'Razza: ' + detenuto.razza;
+                cardDet.querySelector('.coloreOcchi').textContent = 'Colore occhi: ' + detenuto.caratteristicheFisiche.coloreOcchi;
+                cardDet.querySelector('.coloreCapelli').textContent = 'Colore capelli: ' + detenuto.caratteristicheFisiche.coloreCapelli;
+                cardDet.querySelector('.altezza').textContent =  'Altezza: ' + detenuto.caratteristicheFisiche.altezza;
+                cardDet.querySelector('.peso').textContent = 'Peso: ' + detenuto.caratteristicheFisiche.peso;
+
+                return containerDetenuto.appendChild(cardDet);
+            })
+     //   } catch (error) {
+       //     console.error(error);
+        //}
+
+        //VERSIONE ANDREA
+        /* const detenuti = await Promise (urlDet =>{
+            try {
+                return fetch(urlDet).then(response => response.json());
+            } catch (error) {
+                return error;
+            }
+        })
+        console.log(detenuti) */
+        // detenuti.map(detenuto => {
+
+        //     const cardDetenuto = document.importNode(tplDet.content, true);
+        //     // console.log(cardDetenuto)
+        //     cardDetenuto.querySelector('.nome').textContent = detenuto.nomePersonaggio;
 
 
+
+        //     containerDetenuto.append(cardDetenuto);
+        // })
+        
         // const nomeDetenuto;
         // const razzaDetenuto;
         // const caratteristicheDetenuto;
